64c052477b6cb78176cbee3abb14946842c675e2
lemonlite <68134638+lemonlite@users.noreply.github.com>
2022-01-17 18:01:50 -0800

    reupdate

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo "----------Building project:[ IncrementDecrementOperators - Debug ]----------"
-	@cd "IncrementDecrementOperators" && "$(MAKE)" -f  "IncrementDecrementOperators.mk"
-clean:
-	@echo "----------Cleaning project:[ IncrementDecrementOperators - Debug ]----------"
-	@cd "IncrementDecrementOperators" && "$(MAKE)" -f  "IncrementDecrementOperators.mk" clean
--- Section8/LogicalOperators/main.cpp
+++ Section8/LogicalOperators/main.cpp
@@ -4,11 +4,50 @@
 /*  not          //this is a negation    //C++ has 3 logical operators and its symbols this are as below
      !
 =================================================
-    and         // logical and this are like bash scripts "shebang" and && conditions
+    and         // logical "and" only true when both expersions are true anything else is false                                
      &&
 =================================================
     or
-    ||          // logical or
+    ||          // logical "or"  the only time its false is if both expresions are false. if any expressions are true the compiler already knows its true
 ==================================================
 */
 
+#include <iostream> 
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::boolalpha;
+using std::noboolalpha;
+
+
+int main()
+{
+    int num {};
+    const int lower {10};
+    const int upper {20};
+    
+    cout << boolalpha;
+    
+    
+    // Determine if an entered integer is between two other integers
+    // assume lower < upper
+    cout <<"Enter an integer - the bounds are " << lower <<" and " << upper <<": ";
+    cin >> num;
+    
+    bool within_bounds {false};
+    
+    within_bounds = (num > lower && num < upper);
+    cout << num <<" is between " << lower <<" and " << upper <<": " << within_bounds << endl;
+    
+    
+    
+    //Detremine if an entered integer is outside two other integers
+    //assume lower < upper
+//    cout <<"\nEnter an integer - the bounds are " <<lower <<" and " <<upper <<":";
+//    cin >> num;
+    
+    
+    cout << endl;
+    return 0;
+}
