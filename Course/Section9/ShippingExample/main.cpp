1ef1ab787816822ba6b31bea9bb743e914039b28
lemonlite <68134638+lemonlite@users.noreply.github.com>
2022-01-24 08:03:20 -0800

    update

--- Course/Section9/ShippingExample/main.cpp
+++ Course/Section9/ShippingExample/main.cpp
@@ -15,9 +15,45 @@ using std::endl;
 
 int main()
 {
-    
+//     cout <<" ===============================================\n";
+//     cout <<" Welcome to shipping calculator\n";
+//     cout <<" ===============================================\n";
+//     cout <<" Base cost is $2.50.\n";
+//     cout <<" If package volume is greater than 100 cubic inch 10% surcharge\n";
+//     cout <<" greater than 500 cubic inch is 25% surcharge\n";
+//     cout <<" =================================================\n";
 
+//     int length{}, width{}, height{};
+//     double base_cost{2.50};
+//     const int tier1_threshold {100};       //volume
+//     const int tier2_threshold {500};     //volume
+//     int max_dimension_length {10};      //inches
+//     double tier1_surcharge{0.10};   //10% extra
+//     double tier2_surcharge{0.25};   //25% extra
+
+// //All dimension must be 10 inch or less
+
+// int package_volume{};
+
+// cout <<" Enter lenght, width and height of the package: ";
+// cin >> length >> width >> height;
+//     if ( length > max_dimension_length || width > max_dimension_length || height > max_dimension_length)
+//     cout <<" Max dimension exceeded try again.\n";
+//     else
+//     {
+//         double package_cost {};
+//         package_volume = length * width * height;
+//         package_cost = base_cost;
     
+//     if (package_volume > tier2_threshold)
+//        package_cost += package_cost * tier2_surcharge;
+//        cout <<" adding tier 2 surcharge\n";
+//     // else if (package_volume > tier1_threshold)
+//     //        package_cost += package_cost * tier1_surcharge;
+//     //        cout <<" adding tier 1 surcharge\n";
+//      }   
+
+
     
     
     cout << endl;
---
+++ PrinciplesandpracticeusingC++/beginning/session2/exercise5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>       //NOTE! this headers is what the course book uses until chatper 10 by 21 you'll understand it
+#include <vector>
+
+using std::cout;   //i just made this because its bad practice to use namespace in standard  C++ library
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{   //create a double distance from each other cities    
+    // from santa cruz to monterey is 44.6 miles
+
+      vector<double> distance {44.5,44.5};
+
+      double sum =0;
+
+        cout << distance << sum << "\n";
+
+
+
+
+
+
+    cout << endl;
+    return 0;
+}
