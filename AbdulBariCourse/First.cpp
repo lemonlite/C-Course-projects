1963b71bde7af93eea5915f5b211d3e34dfcbd9f
lemon_lite <xigszz56@ArchDesk.localdomain>
2022-02-19 18:07:42 -0800

    modified:   AbdulBariCourse/First.cpp 	modified:   PrinciplesandpracticeusingC++/Sessions/Session4.cpp

--- AbdulBariCourse/First.cpp
+++ AbdulBariCourse/First.cpp
@@ -7,8 +7,7 @@ using namespace std;
 
 int main()
 {
-        string name1 = " johnson ";
-        cout << name1[3,5];
+       
 
 
     cout << endl;
---
+++ PrinciplesandpracticeusingC++/Sessions/Session4.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+
+void exercise1()
+{
+    
+
+}
+
+
+
+
+
+int main()
+{
+
+    
+    return 0;
+}
