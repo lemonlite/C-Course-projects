a91f1a70791fae68a4c7836f0e53915f9d1c07ee
lemonlite <68134638+lemonlite@users.noreply.github.com>
2022-02-24 00:02:43 -0800

    u

--- Workshop/macros.cpp
+++ Workshop/macros.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-
+#include <cstdlib>
+#define pi 3.141         //macros are known as preprocessor directive means, something picked up by the preprocessor "#" while compiling here in example user made pi and will replace anything that is pi
+#define Multiply(a,b) (a * b)
 
 int main()
 {
+    
+    
 
-
-
-    std::cout << std::endl;
+    std::cout << std::endl; // here macro is called hence will output 3.141
     return 0;
 }
