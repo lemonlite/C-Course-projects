bae55a35246aed0774b6596a56792ed40a6049e3
lemon_lite <xigszz56@ArchDesk.localdomain>
2022-03-03 18:33:53 -0800

    deleted:    PrinciplesandpracticeusingC++/.vs/PrinciplesandpracticeusingC++/project-colors.json 	deleted:    PrinciplesandpracticeusingC++/.vs/PrinciplesandpracticeusingC++/v17/.suo 	deleted:    PrinciplesandpracticeusingC++/.vs/PrinciplesandpracticeusingC++/v17/Browse.VC.db 	deleted:    PrinciplesandpracticeusingC++/.vs/PrinciplesandpracticeusingC++/v17/ipch/AutoPCH/6bbb0f8c8f472e88/EXERCISE3.ipch 	deleted:    PrinciplesandpracticeusingC++/.vs/PrinciplesandpracticeusingC++/v17/ipch/AutoPCH/cc121ed0b520a4d8/EXERCISE3.ipch 	deleted:    PrinciplesandpracticeusingC++/.vs/PrinciplesandpracticeusingC++/v17/ipch/AutoPCH/dab138e18626aa41/EXERCISE4.ipch 	deleted:    PrinciplesandpracticeusingC++/.vs/ProjectSettings.json 	deleted:    PrinciplesandpracticeusingC++/.vs/VSWorkspaceState.json 	deleted:    PrinciplesandpracticeusingC++/.vs/slnx.sqlite 	deleted:    PrinciplesandpracticeusingC++/.vscode/tasks.json 	new file:   PrinciplesandpracticeusingC++/Notes/StagesOfDevelopment.txt 	new file:   PrinciplesandpracticeusingC++/Notes/Stratgy.txt 	new file:   PrinciplesandpracticeusingC++/Notes/USE stdlibheader.txt 	modified:   PrinciplesandpracticeusingC++/Sessions/Session4.cpp 	deleted:    PrinciplesandpracticeusingC++/Sessions/USE stdlibheader.txt 	modified:   cpluscourse.code-workspace

--- PrinciplesandpracticeusingC++/Sessions/Session4.cpp
+++ PrinciplesandpracticeusingC++/Sessions/Session4.cpp
@@ -1,18 +1,47 @@
 #include "std_lib_facilities.h"
 
 
-void exercise1()
+void CleanerCalculator()
 {
-    
+    cout <<"Please enter expression +.-,*,/\n";
+    cout <<" ad an x to end expression (ex.1+2+1): ";
+    int lval = 0;
+    int rval;
+    cin >> lval;
+    if (!cin) error("no first operand");
+    for (char op; cin >>op; )
+    {
+        if (op=='x') cin >> rval;
+        if (!cin) error("no second operand" ); 
+        switch(op)
+        {
+            case '+':
+                lval += rval;   //add assignment operator
+                break;
+            case '-':
+            lval -= rval;       //subtract assignment operator
+                break;
+            case '*':
+            lval *= rval;
+                break;
+            case '/':
+            lval /= rval;
+                break;
+            default:
+                cout <<"Result: " << lval <<"\n";
+                
+        }
+    }
 
+    error("bad expression");
 }
-
 
 
 
 
 int main()
 {
+    CleanerCalculator();
 
     
     return 0;
